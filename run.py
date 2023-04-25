58daadf874a45f1a98e8603e57e5039d3a825c16
Rafael Carreiro <carreirorco@gmail.com>
2023-04-25 14:50:26 -0300

    feat: download video or audio from Youtube

---
+++ run.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from pytube import Youtube
+
+st.title("Youtube Downloader")
+st.subheader("Insira a URL do vídeo: ")
+url = st.text_input("URL: ")
+
+if url != "":
+    yt = Youtube(url)
+    st.image(yt.thumbnail_url, width=300)
+    st.subheader("""
+    {}
+    # Lenght: {} seconds
+    # Rating: {}
+    """.format(yt.title, yt.length, yt.rating))
+
+    video = yt.streams
+    if len.video > 0:
+        downloaded, download_video = False, False
+        download_video = st.button("Baixar vídeo")
+
+        if yt.streams.filter(only_audio=True):
+            download_audio = st.button("Baixar somente o audio")
+        if download_video:
+            video.get_lowest_resolution().download()
+            downloaded = True
+        if download_audio:
+            video.download(only_audio=True).first().download()
+            downloaded = True
+        if downloaded:
+            st.subheader("Download Concluído!")
+    else:
+        st.subheader("Não foi possível baixar o vídeo")
